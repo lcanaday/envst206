a10615697e2c73ea0d81c3e9dabd2383c5c8b46a
Lindsay Canaday <lindsaycanaday@lindsays-mbp.hamilton.edu>
2020-09-12 15:58:14 -0400

    started activity 3

---
+++ activity 3/activity3_script.R
@@ -0,0 +1,70 @@
+# Activity 3
+
+# read in lemming data
+ch4 <- read.csv("/Users/lindsaycanaday/Documents/a03/lemming_herbivory.csv")
+# change herbivory to factor
+ch4$herbivory <- as.factor(ch4$herbivory)
+
+# boxplot of ch4 fluxes by treatment
+plot(ch4$CH4_Flux ~ ch4$herbivory, xlab = "Treatment", ylab = "CH4 fluxes (mgC m -2 day-1)")
+
+# test normality of data in each treatment
+# shapiro-wilk test on ctl plots
+shapiro.test(ch4$CH4_Flux[ch4$herbivory == "Ctl"])
+# shapiro-wilk test on ex plots
+shapiro.test(ch4$CH4_Flux[ch4$herbivory == "Ex"])
+
+# test equal variance
+# bartlett test
+bartlett.test(ch4$CH4_Flux ~ ch4$herbivory)
+
+## Q1 - two sample t test for CH4 flux and herbivory
+t.test(ch4$CH4_Flux ~ ch4$herbivory)
+
+## Q2 - one sided t test arguments
+help(t.test)
+# example one-sided t test
+t.test(ch4$CH4_Flux ~ ch4$herbivory, mu = 0, alternative = "less")
+
+# read in insect data
+datI <- read.csv("/Users/lindsaycanaday/Documents/a03/insect_richness.csv")
+# change urbanName to factor
+datI$urbanName <- as.factor(datI$urbanName)
+levels(datI$urbanName)
+
+## Q4 - test assumptions of anova for insect data
+# test normality of data in each group
+# shapiro-wilk test on dense
+shapiro.test(datI$Richness[datI$urbanName == "Dense"])
+# shapiro-wilk test on developed
+shapiro.test(datI$Richness[datI$urbanName == "Developed"])
+# shapiro-wilk test on natural
+shapiro.test(datI$Richness[datI$urbanName == "Natural"])
+# shapiro-wilk test on suburban
+shapiro.test(datI$Richness[datI$urbanName == "Suburban"])
+# test equal variance
+# bartlett test
+bartlett.test(datI$Richness ~ datI$urbanName)
+
+# boxplot of richness data by group to visualize data
+plot(datI$Richness ~ datI$urbanName, xlab = "Urban Environment Type", ylab = "Richness")
+
+# running anova test on insect data
+# specify model for species richness and urban type
+in.mod <- lm(datI$Richness ~ datI$urbanName)
+# run the ANOVA
+in.aov <- aov(in.mod)
+# print ANOVA table
+summary(in.aov)
+# posthoc test to verify differences
+# run Tukey HSD
+tukeyT <- TukeyHSD(in.aov)
+# view results
+tukeyT
+# plot Tukey HSD results
+# make axes labels smaller to fit
+plot(tukeyT, cex.axis = 0.75)
+
+
+
+
