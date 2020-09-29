5866895f170b8761328ee6126e16d85bac51a221
Lindsay Canaday <lindsaycanaday@lindsays-mbp.hamilton.edu>
2020-09-29 15:38:57 -0400

    activity 5 progress

--- activity5/activity5_script.R
+++ activity5/activity5_script.R
@@ -25,6 +25,8 @@ ca <- pr[pr$NAME == nameS[2], ]
 ny <- pr[pr$NAME == nameS[5], ]
 # plot of ca precip
 plot(ca$year, ca$totalP)
+
+## Q2 - general trends in annual precip ca vs ny
 # improve plot of ca precip
 plot(ca$year, ca$totalP,
      type = "b",
@@ -51,3 +53,40 @@ legend("topleft",
        lwd = 1,
        bty = "n")
 
+## Q3 - plot mean annual max temp ny vs nd
+# dataframe with just max temp, year, site
+# remove missing
+datT <- na.omit(data.frame(NAME=datW$NAME,
+                           year=datW$year,
+                           TMAX=datW$TMAX))
+# mean annual max temp
+maxtemp <- aggregate(datT$TMAX, by=list(datT$NAME, datT$year), FUN="mean", na.rm=TRUE)
+# rename columns
+colnames(maxtemp) <- c("NAME", "year", "TMAX")
+# add column ncount
+maxtemp$ncount <- aggregate(datT$TMAX, by=list(datT$NAME, datT$year), FUN="length")$x
+# new dataframe only years >=364 observations
+mt <- maxtemp[maxtemp$ncount >= 364, ]
+# look only at morrisville and mandan
+mny <- mt[mt$NAME == nameS[5], ]
+mnd <- mt[mt$NAME == nameS[3], ]
+# plot of mny mean annual max temp
+plot(mny$year, mny$TMAX,
+     type = "b",
+     pch = 19,
+     ylab = "Mean annual maximum temperature (C)",
+     xlab = "Year",
+     main = "Trends in Mean Annual Maximum Temperature",
+     yaxt = "n",
+     ylim = c(8,15))
+axis(2, seq(8,15, by=1), las=2)
+points(mnd$year, mnd$TMAX,
+       type = "b",
+       pch = 19,
+       col = "tomato3")
+legend("bottomright",
+       c("Morrisville, NY", "Mandan, ND"),
+       col = c("black", "tomato3"),
+       pch = 19,
+       lwd = 1,
+       bty = "n")
