9e9ab88e828c017b78875433eecfa9253b62f1b5
Lindsay Canaday <lindsaycanaday@Lindsays-MacBook-Pro.local>
2020-10-11 08:43:09 -0400

    installed packages

---
+++ activity6/activity6_script.R
@@ -0,0 +1,10 @@
+# Activity 6
+
+# install packages
+install.packages(c("sp", "rgdal", "dplyr"))
+# package for vector data
+library(sp)
+# package for reading in spatial data
+library(rgdal)
+# data management package
+library(dplyr)
