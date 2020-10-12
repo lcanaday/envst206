02f3ba775b72434c46f20cc16b2b4c7bd8a24852
Lindsay Canaday <lindsaycanaday@Lindsays-MacBook-Pro.local>
2020-10-12 16:54:23 -0400

    read in data for activity 6

--- activity6/activity6_script.R
+++ activity6/activity6_script.R
@@ -1,10 +1,14 @@
 # Activity 6
 
 # install packages
-install.packages(c("sp", "rgdal", "dplyr"))
+# install.packages(c("sp", "rgdal", "dplyr"))
 # package for vector data
 library(sp)
 # package for reading in spatial data
 library(rgdal)
 # data management package
 library(dplyr)
+
+# read in shapefiles
+g1966 <- readOGR("/Users/lindsaycanaday/Documents/a06/GNPglaciers/GNPglaciers_1966.shp")
+g2015 <- readOGR("/Users/lindsaycanaday/Documents/a06/GNPglaciers/GNPglaciers_2015.shp")
