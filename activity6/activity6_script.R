3b4db8613f629e57bf94999f4cf1e6695da56128
Lindsay Canaday <lindsaycanaday@Lindsays-MacBook-Pro.local>
2020-10-13 18:16:46 -0400

    completed activity 6

--- activity6/activity6_script.R
+++ activity6/activity6_script.R
@@ -62,3 +62,54 @@ spplot(g1966, "gdiff", main = "% change in area", col = "transparent")
 # look at vulture glacier in 1966
 vulture66 <- g1966[g1966@data$GLACNAME == "Vulture Glacier", ]
 plot(vulture66, main = "Vulture Glacier in 1966", col = "slategray")
+
+## Q8 - describing glacial loss in Glacier National Park
+# mean and stddev of % loss
+mean(g1966$gdiff)
+sd(g1966$gdiff)
+# glaciers w/ largest and smallest % loss
+max(g1966$gdiff)
+g1966$GLACNAME[g1966$gdiff == max(g1966$gdiff)]
+g1966$area66[g1966$gdiff == max(g1966$gdiff)]
+min(g1966$gdiff)
+g1966$GLACNAME[g1966$gdiff == min(g1966$gdiff)]
+g1966$area66[g1966$gdiff == min(g1966$gdiff)]
+# % loss of glaciers w/ largest and smallest 1966 area
+max(g1966$area66)
+g1966$GLACNAME[g1966$area66 == max(g1966$area66)]
+g1966$gdiff[g1966$area66 == max(g1966$area66)]
+min(g1966$area66)
+g1966$GLACNAME[g1966$area66 == min(g1966$area66)]
+g1966$gdiff[g1966$area66 == min(g1966$area66)]
+
+## Q9 - footprint maps of glaciers with largest and smallest % loss
+# look at glacier w/ max % loss in 1966
+maxloss66 <- g1966[g1966@data$gdiff == max(g1966$gdiff),]
+# join data w/ spatial data in 2015
+g2015@data <- left_join(g2015@data, gAll, by = "GLACNAME")
+# look at glacier w/ max % loss in 2015
+maxloss15 <- g2015[g2015@data$gdiff == max(g2015$gdiff),]
+# plot glacier with max loss
+plot(maxloss66, col="slategray",
+     main = "Boulder Glacier extent:
+     greatest % loss between 1966 and 2015")
+plot(maxloss15, col="tomato3", add=TRUE)
+legend("topright",
+       c("1966", "2015"),
+       col = c("slategray", "tomato3"),
+       pch = 19,
+       bty = "n")
+# look at glacier w/ min % loss in 1966
+minloss66 <- g1966[g1966@data$gdiff == min(g1966$gdiff),]
+# look at glacier w/ min % loss in 2015
+minloss15 <- g2015[g2015@data$gdiff == min(g2015$gdiff),]
+# plot glacier with min loss
+plot(minloss66, col = "slategray",
+     main = "Pumpelly Glacier extent:
+     smallest % loss between 1966 and 2015")
+plot(minloss15, col = "tomato3", add=TRUE)
+legend("topright",
+       c("1966", "2015"),
+       col = c("slategray", "tomato3"),
+       pch = 19,
+       bty = "n")
