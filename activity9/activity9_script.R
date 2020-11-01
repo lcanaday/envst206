64850260ed5c95b9f415ebb9b07f7b07d88ae034
Lindsay Canaday <lindsaycanaday@Lindsays-MacBook-Pro.local>
2020-11-01 16:40:25 -0500

    activity 9 progress

--- activity9/activity9_script.R
+++ activity9/activity9_script.R
@@ -7,3 +7,29 @@ library(sp)
 library(rgdal)
 library(caret)
 library(randomForest)
+
+# set up directory for oneida data folder
+dirR <- "/Users/lindsaycanaday/Documents/a08/oneida"
+# read in sentinel data
+rdatB2 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B02_20m.tif"))
+rdatB3 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B03_20m.tif"))
+rdatB4 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B04_20m.tif"))
+rdatB5 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B05_20m.tif"))
+rdatB6 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B06_20m.tif"))
+rdatB7 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B07_20m.tif"))
+rdatB8 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B08_20m.tif"))
+rdatB11 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B11_20m.tif"))
+rdatB12 <- raster(paste0(dirR,"/sentinel/T18TVN_20190814T154911_B12_20m.tif"))
+clouds <- raster(paste0(dirR,"/sentinel/MSK_CLDPRB_20m.tif"))
+
+# stack all raster data
+allbands <- stack(rdatB2,rdatB3,rdatB4,rdatB5,rdatB6,rdatB7,rdatB8,rdatB11,rdatB12,clouds)
+
+# read in validation data
+algae <- readOGR(paste0(dirR,"/Oneida/algae.shp"), verbose=FALSE)
+agri <- readOGR(paste0(dirR,"/Oneida/agriculture.shp"), verbose=FALSE)
+built <- readOGR(paste0(dirR,"/Oneida/built.shp"), verbose=FALSE)
+forest <- readOGR(paste0(dirR,"/Oneida/forest.shp"), verbose=FALSE)
+water <- readOGR(paste0(dirR,"/Oneida/water.shp"), verbose=FALSE)
+wetlands <- readOGR(paste0(dirR,"/Oneida/wetlands.shp"), verbose=FALSE)
+
