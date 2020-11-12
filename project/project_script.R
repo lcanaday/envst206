9737fe6fa156f9392154089db8fd2062e110398f
Lindsay Canaday <lindsaycanaday@Lindsays-MacBook-Pro.local>
2020-11-12 14:13:34 -0500

    project work

--- project/project_script.R
+++ project/project_script.R
@@ -4,6 +4,8 @@
 library(sp)
 library(rgdal)
 library(dplyr)
+# install.packages("rgeos")
+library(rgeos)
 
 # read in data
 seaice <- readOGR("/Users/lindsaycanaday/Documents/sea_ice_all/sea_ice_all.shp")
@@ -12,3 +14,9 @@ seaice <- readOGR("/Users/lindsaycanaday/Documents/sea_ice_all/sea_ice_all.shp")
 plot(seaice)
 head(seaice@data)
 plot(seaice[seaice@data$year == 1979,])
+
+# calculate area of sea ice extent for year
+gArea(seaice[seaice@data$year == 1979,])
+gArea(seaice[seaice@data$year == 2019,])
+
+# create table of year and area
